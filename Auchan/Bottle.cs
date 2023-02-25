4c84351acb489a193b2e711637e0417ba9615d5c
Timur <Timur@TIMURSLAPTOP>
2023-02-25 11:22:12 +0700

    Добавьте файлы проекта.

---
+++ Auchan/Bottle.cs
@@ -0,0 +1,41 @@
+﻿public class Bottle
+{
+    private float capacity;
+    private string brand;
+    private float price;
+    private string type;
+    public float Price
+    {
+        get => Price;      
+    }
+    public string Type
+    {
+        get => Type;    
+    }
+    public float Capacity
+    {
+        get => Capacity;
+    }
+    public string Brand
+    {
+        get => Brand;   
+    }
+
+    public Bottle(string brand = "No brand", string type = "Undefined", float capacity = 0.0f, float price = 0.0f)
+    {
+        this.brand = brand;
+        this.type = type;
+        this.capacity = capacity;
+        this.price = price;
+    }
+    public string GetInfo()
+    {
+        string s = string.Format($"1. Производитель: {brand}\n2. Тип: {type}\n3. Объем: {capacity}\n4. Цена: {price}\n");
+        return s;
+    }
+    public void BuyWater()
+    {
+        Console.WriteLine("Бутылка кулена");
+    }
+}
+
---
+++ Auchan/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Bottle bottle = new Bottle();
+        Console.WriteLine(bottle.GetInfo());
+
+        string inBrand = Console.ReadLine();
+        string inType = Console.ReadLine();
+        float inCapacity = float.Parse(Console.ReadLine());
+        float inPrice = float.Parse(Console.ReadLine());
+
+        Bottle bottle2 = new Bottle(inBrand, inType, inCapacity, inPrice);
+        Console.WriteLine(bottle2.GetInfo());
+
+        int size = int.Parse(Console.ReadLine());
+        Bottle[] array = new Bottle[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            inBrand = Console.ReadLine();
+            inType= Console.ReadLine();
+            inCapacity= int.Parse(Console.ReadLine());
+            inPrice= float.Parse(Console.ReadLine());
+
+            array[i] = new Bottle(inBrand, inType, inCapacity, inPrice);
+        }
+        foreach (var item in array) Console.WriteLine(item.GetInfo());
+        foreach (var item in array)
+        {
+            if (item.Brand == "Карачинаская" && item.Price < 100.0f)
+            {
+                item.BuyWater();
+                Console.WriteLine(item.GetInfo());
+            }
+        }
+
+
+    }
+}
